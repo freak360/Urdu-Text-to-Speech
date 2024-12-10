2e82b3a7d79e8a0faaf5b9a68c509e098857c670
Aneeb Ajmal <maneebajmal@gmail.com>
2024-12-10 22:14:35 +0500

    Added more voices

--- app.py
+++ app.py
@@ -107,18 +107,70 @@ def text_to_speech_gul(subscription_key, region, text, file_name):
             if cancellation_details.error_details:
                 print("Error details: {}".format(cancellation_details.error_details))
 
+def text_to_speech_ava(subscription_key, region, text, file_name):
+    # Set up the speech configuration with your subscription key and service region
+    speech_config = speechsdk.SpeechConfig(subscription=subscription_key, region=region)
+    speech_config.speech_synthesis_voice_name = "en-US-AvaMultilingualNeural"
+    
+    # Specify the audio output file
+    audio_config = speechsdk.audio.AudioOutputConfig(filename=file_name)
+    
+    # Set up the text-to-speech synthesizer with the audio output configuration to save to a file
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+    
+    # Synthesize the text
+    result = speech_synthesizer.speak_text_async(text).get()
+    
+    # Check the result
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        print(f"Speech synthesized to the file {file_name} for text [{text}]")
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            if cancellation_details.error_details:
+                print("Error details: {}".format(cancellation_details.error_details))
+
+def text_to_speech_andrew(subscription_key, region, text, file_name):
+    # Set up the speech configuration with your subscription key and service region
+    speech_config = speechsdk.SpeechConfig(subscription=subscription_key, region=region)
+    speech_config.speech_synthesis_voice_name = "en-US-AndrewMultilingualNeural"
+    
+    # Specify the audio output file
+    audio_config = speechsdk.audio.AudioOutputConfig(filename=file_name)
+    
+    # Set up the text-to-speech synthesizer with the audio output configuration to save to a file
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+    
+    # Synthesize the text
+    result = speech_synthesizer.speak_text_async(text).get()
+    
+    # Check the result
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        print(f"Speech synthesized to the file {file_name} for text [{text}]")
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            if cancellation_details.error_details:
+                print("Error details: {}".format(cancellation_details.error_details))
+
 
 
 def get_audio(text, speaker):
     output_file = "speech.mp3"
-    if speaker == "Asad":
+    if speaker == "Asad(ur)":
         text_to_speech_asad(subscription_key, region, text, output_file)
-    if speaker == "Uzma":
+    if speaker == "Uzma(ur)":
         text_to_speech_uzma(subscription_key, region, text, output_file)
-    if speaker == "Salman":
+    if speaker == "Salman(ur)":
         text_to_speech_salman(subscription_key, region, text, output_file)
-    if speaker == "Gul":
+    if speaker == "Gul(ur)":
         text_to_speech_gul(subscription_key, region, text, output_file)
+    if speaker == "Ava(en)":
+        text_to_speech_ava(subscription_key, region, text, output_file)
+    if speaker == "Andrew(en)":
+        text_to_speech_andrew(subscription_key, region, text, output_file)
 
     st.audio(output_file)
 
@@ -133,9 +185,9 @@ def save_audio_stream(stream, output_file):
 
 def main():
     st.set_page_config(page_title="Text to Speech", page_icon="✨")
-    st.header("Ivac's Text to Speech App")
+    st.header("Text to Speech App")
     text = st.text_area("Input Urdu Text Here")
-    speaker = st.radio("Select Voice", ["Asad", "Uzma", "Salman", "Gul"])
+    speaker = st.radio("Select Voice", ["Asad(ur)", "Uzma(ur)", "Salman(ur)", "Gul(ur)", "Ava(en)", "Andrew(en)"])
     if st.button("Generate"):
         get_audio(text, speaker)
 
